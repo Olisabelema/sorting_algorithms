edefb70de065964747d9d8af173916f170d2c421
olisabelema <olisabelema@gmail.com>
2023-06-27 14:20:51 -0700

    updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,81 +1,85 @@
 #include "sort.h"
-
-void my_quick_sort(int *array, int start, int end, int whole_size);
-
+void merge(int *leftArr, int leftSize, int *rightArr,
+		   int rightSize, int *array);
 /**
- * partition - lomuto partition scheme
- * for sorting an array.
- * @array: array to be sorted
- * @start: starting index of the array
- * @end: ending index of the array
- * @whole_size: size of whole array
- * Return: location of the pivot
- */
-int partition(int *array, int start, int end, int whole_size)
-{
-	int partition = start - 1;
-	int pivot = array[end];
-	int i = 0, temp;
-
-	for (i = start; i < end; i++)
-	{
-		if (array[i] < pivot)
-		{
-			partition++;
-			if (array[i] != array[partition])
-			{
-				temp = array[i];
-				array[i] = array[partition];
-				array[partition] = temp;
-				print_array(array, whole_size);
-			}
-		}
-	}
-	if (array[partition + 1] != array[end])
-	{
-		temp = array[partition + 1];
-		array[partition + 1] = array[end];
-		array[end] = temp;
-		print_array(array, whole_size);
-	}
-
-	return (partition + 1);
-}
-
-/**
- * quick_sort - Quick sort algorithm to
- * sort an array in ascending order.
- * @array: array to be sorted
- * @size: size of the array
+ * merge_sort - merge sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
  * Return: void
  */
-void quick_sort(int *array, size_t size)
+void merge_sort(int *array, size_t size)
 {
-	int start = 0;
-	int end = size - 1;
 
-	if (size <= 1 || array == NULL)
+	size_t mid = size / 2;
+	size_t i = 0;
+	int leftSize = mid;
+	int rightSize = size - mid;
+	int leftArr[4096];
+	int rightArr[4096];
+
+	if (size < 2)
 		return;
 
-	my_quick_sort(array, start, end, size);
+	for (i = 0; i < mid; i++)
+	{
+		leftArr[i] = array[i];
+	}
+	for (i = mid; i < size; i++)
+	{
+		rightArr[i - mid] = array[i];
+	}
+
+	merge_sort(leftArr, leftSize);
+	merge_sort(rightArr, rightSize);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(leftArr, leftSize);
+	printf("[right]: ");
+	print_array(rightArr, rightSize);
+	merge(leftArr, leftSize, rightArr, rightSize, array);
+	printf("[Done]: ");
+	print_array(array, size);
 }
 
 /**
- * my_quick_sort - Implementation of the 
- * Quick sort algorithm to sort an array.
- * @array: array to be sorted
- * @start: start index of the array
- * @end: end index of the array
- * @whole_size: size of the entire array
- * Return: void
+ * merge - merge function in merge_sort
+ * @leftArr: left array
+ * @leftSize: size of left array
+ * @rightArr: right array
+ * @rightSize: size of right array
+ * @array: array to merge in place
  */
-void my_quick_sort(int *array, int start, int end, int whole_size)
+void merge(int *leftArr, int leftSize, int *rightArr,
+		   int rightSize, int *array)
 {
-	if (start < end)
-	{
-		int partition_index = partition(array, start, end, whole_size);
+	int i = 0, j = 0, k = 0;
 
-		my_quick_sort(array, start, partition_index - 1, whole_size);
-		my_quick_sort(array, partition_index + 1, end, whole_size);
+	while (i < leftSize && j < rightSize)
+	{
+		if (leftArr[i] <= rightArr[j])
+		{
+			array[k] = leftArr[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			array[k] = rightArr[j];
+			k++;
+			j++;
+		}
+	}
+	while (i < leftSize)
+	{
+		array[k] = leftArr[i];
+		k++;
+		i++;
+	}
+	while (j < rightSize)
+	{
+		array[k] = rightArr[j];
+		k++;
+		j++;
 	}
 }
